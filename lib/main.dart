20a9dfe10162899a119521897dff2fa6be151fba
arun <nuranisafirdausy1201@gmail.com>
2022-03-17 13:19:49 +0700

    rev

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_parcel_app/app.dart';
-import 'package:flutter_parcel_app/parcel_app_theme.dart';
-import 'package:flutter_parcel_app/ui/screens/home_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
